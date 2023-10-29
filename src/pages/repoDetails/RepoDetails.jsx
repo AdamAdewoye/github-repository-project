6725e3f471b3dbe5883f725790df939e26908395
The First Hack <firstadamadewoye@gmail.com>
2023-10-29 11:48:53 -0700

    To update

--- src/pages/repoDetails/RepoDetails.jsx
+++ src/pages/repoDetails/RepoDetails.jsx
@@ -1,12 +1,55 @@
-import React from 'react'
+// import React from 'react'
 import ErrorBoundary from '../../components/ErrorBoundary/ErrorBoundary'
 
 const RepoDetails = () => {
   return (
     <ErrorBoundary>
-    <div>
-      Repo Details
-    </div>
+    <div>Single Repo
+            {
+            
+            /* <div className="single">
+ 
+             <div className="single__header">
+                   
+                <div     >
+
+                    <img src={avatar_url} className={styles.img}/>
+                    <p>{login}</p>
+                </div>
+                <div className={styles.time}><p><Clock/></p><p>{format(updated_at)}</p></div>
+                 
+             </div>
+             <div className="single__body">
+
+                        <p className="single__name">{description ?<p>{description}</p>:<p>{name} project</p>}</p>
+                        
+                        
+                        <p className="single__socials">
+                                <BiGitRepoForked className='single__icons'/>:
+                                <p>{forks}</p>
+
+                                
+                            </p>
+                            <p>
+                            <p className="single__socials">
+                                <Link className='single__icons'/>
+
+                                <NavLink to={html_url} className="single__link">{html_url} </NavLink>
+                            </p>
+
+                        </p>
+                        {language === "JavaScript"?
+                        <>
+                        <p><Language/>Html and css</p>
+                        <p><Language/>{language}</p>
+                        </>
+                        :language && <p><Language/>{language}</p>}
+                        {!language && <p><Language/> programmer</p> }
+                       
+
+                    </div>
+            </div> */}
+        </div>
     </ErrorBoundary>
   )
 }
